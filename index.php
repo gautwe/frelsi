43b92f3bac2309ddc9df3f9dbd98b0354d424124
gautwe <me@gautwe.com>
2015-02-09 23:11:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-  // First, include what we need.
-  include("inc/header.php");
-  include("inc/top.php");
-  include("inc/lang.php");
-  
-  // With lang.php, we need to redirect to a ?sprak.
-  if(!asset($_GET['sprak'])){header('Location: index?sprak=en');}
-  
+  // What we need.
+	include("inc/header.php");
+	include("inc/top.php");
+	// if we are logged, go to member.php
+	if(isset($_SESSION['login'])){header('Location: '.$p_member.'');}
+?>
+<!-- the content is fixed on the container -->
+<div id="container">
+	<header id="container_top">
+		<p><?php echo($l_hello); ?></p> <-- the arg is fixed with the lang file -->
+	</header>
+
+</div>
